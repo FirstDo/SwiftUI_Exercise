08774fa6ab93a04ea3b3a35c5095979f2dc30c48
doyeonKim <whqtkf12@naver.com>
2024-11-03 17:33:10 +0900

    Day1: OptionalModifierView

--- SwiftUI-Tips/SwiftUI-Tips/Subject/OptionalModifierView.swift
+++ SwiftUI-Tips/SwiftUI-Tips/Subject/OptionalModifierView.swift
@@ -1,14 +1,44 @@
 import SwiftUI
 
+enum InternetState: String, CaseIterable {
+  case loading
+  case success
+  case error
+}
+
 struct OptionalModifierView: View {
+  @State private var state: InternetState = .loading
+  
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    VStack(spacing: 30) {
+      Picker("", selection: $state) {
+        ForEach(InternetState.allCases, id: \.rawValue) {
+          Text($0.rawValue)
+            .tag($0)
+        }
+      }
+      .pickerStyle(.segmented)
+      
+      Image(systemName: "wifi.circle.fill")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 50, height: 50)
+        .modifiers {
+          switch state {
+          case .loading: $0.foregroundStyle(.blue)
+          case .success: $0.foregroundStyle(.green)
+          case .error: $0.foregroundStyle(.red)
+          }
+        }
     }
-    .padding()
+  }
+}
+
+extension View {
+  func modifiers<Content: View>(
+    @ViewBuilder content: @escaping (Self) -> Content
+  ) -> some View {
+    content(self)
   }
 }
 
